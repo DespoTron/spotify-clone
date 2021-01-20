ae0dd2e14f28272883dfebd09b1cbab06cc8e49b
Thai Do <thai.hl.do@gmail.com>
2021-01-19 17:55:18 -0800

    Create login component

---
+++ src/components/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+export default () => {
+  return (
+    <div className="login">
+      {/* Spotify Logo */}
+      {/* Login with Spotify button Logo */}
+    </div>
+  )
+}
