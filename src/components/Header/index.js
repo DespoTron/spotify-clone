d7ac8961a29ced364c759628ea398097a6418897
Thai Do <thai.hl.do@gmail.com>
2021-01-20 15:56:58 -0800

    Add user photo and removed input outline in search bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import './styles.css';
 import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from 'DataLayer';
 
 export default () => {
+  const [{ user }, dispatch] = useDataLayerValue();
+
   return (
     <div className="header">
       <div className="header__left">
         <SearchIcon />
-        <input placeholder="Search for Artists, Songs" type="text" />
+        <input
+          placeholder="Search for Artists, Songs, or Podcasts"
+          type="text"
+        />
       </div>
 
       <div className="header__right">
-        <Avatar src="" alt="TD" />
-        <h4>Thai Do</h4>
+        <Avatar src={user?.images[0]?.url} alt={user} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
