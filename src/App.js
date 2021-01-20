4ddb66ba31f433831a97f87be709793091c725ef
Thai Do <thai.hl.do@gmail.com>
2021-01-19 19:06:04 -0800

    Got spotify to handle login and some basic logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { getTokenFromUrl } from './service/spotify'
 import './App.css'
 import { Login } from './components'
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ''
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('I HAVE A TOKEN -> ', token)
+  }, [])
+
   return (
-    <div className="app">
-      <Login />
-    </div>
+    // BEM
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   )
 }
 
