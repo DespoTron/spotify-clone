4418440a00e9e9679b2b93989dd089967af40b83
Thai Do <thai.hl.do@gmail.com>
2021-01-20 16:49:59 -0800

    Add material ui icons and styled them

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './styles.css';
 import { Header } from '../../components';
 import { useDataLayerValue } from 'DataLayer';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 export default ({ spotify }) => {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -16,6 +19,15 @@ export default ({ spotify }) => {
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
+        </div>
+
+        <div className="body__songs">
+          <div className="body__icons">
+            <PlayCircleFilledIcon className="body__shuffle" />
+            <FavoriteIcon fontSize="large" />
+            <MoreHorizIcon />
+          </div>
+          {/* List of songs */}
         </div>
       </div>
     </div>
