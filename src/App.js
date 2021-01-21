b9c488bcf6911f2d18d069ac2b18375d0c578db8
Thai Do <thai.hl.do@gmail.com>
2021-01-20 16:41:10 -0800

    Add logic for body component to pull user profile and discover weekly

--- src/App.js
+++ src/App.js
@@ -37,6 +37,13 @@ function App() {
           playlists: playlists,
         });
       });
+
+      spotify.getPlaylist('37i9dQZEVXcSu9RzgfqQF1').then((response) => {
+        dispatch({
+          type: 'SET_DISCOVER_WEEKLY',
+          discover_weekly: response,
+        });
+      });
     }
   }, []);
 
--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './styles.css';
 import { Header } from '../../components';
+import { useDataLayerValue } from 'DataLayer';
 
 export default ({ spotify }) => {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
+
   return (
     <div className="body">
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
