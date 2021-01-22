b7b22f8c030a68cce04e8cc2a9ff06d3fecd8758
Thai Do <thai.hl.do@gmail.com>
2021-01-21 16:40:52 -0800

    Update header component for avatar material ui

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,7 +18,7 @@ export default () => {
       </div>
 
       <div className="header__right">
-        <Avatar src={user?.images[0]?.url} alt={user} />
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
         <h4>{user?.display_name}</h4>
       </div>
     </div>
