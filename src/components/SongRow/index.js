be679090b933a1072009228c1698fefdcb8a2929
Thai Do <thai.hl.do@gmail.com>
2021-01-22 16:21:25 -0800

    Update song row component

--- src/components/SongRow/index.js
+++ src/components/SongRow/index.js
@@ -1,16 +1,51 @@
+import { useDataLayerValue } from 'data/DataLayer';
+import { useSoundLayerValue } from 'data/SoundLayer';
 import React from 'react';
 import './styles.css';
+import { useDataLayerValue, useSoundLayerValue } from 'data';
 
 export default ({ track }) => {
+  const [{}, dispatch] = useDataLayerValue();
+  const [{ playing, repeat }, soundDispatch] = useSoundLayerValue();
+
+  const changeTrack = (e, track) => {
+    dispatch({
+      type: 'SET_TRACK',
+      track: track,
+    });
+
+    let wasPlaying = playing;
+    soundDispatch({
+      type: 'SET_PLAYING',
+      playing: false,
+    });
+
+    let audio = new Audio(track.preview_url);
+    audio.loop = repeat;
+    soundDispatch({
+      type: 'SET_AUDIO',
+      audio: audio,
+    });
+
+    if (wasPlaying) {
+      soundDispatch({
+        type: 'SET_PLAYING',
+        playing: true,
+      });
+    }
+
+    document.title = `${track.name} · ${track.artists
+      .map((artist) => artist.name)
+      .join(', ')}`;
+  };
+
   return (
-    <div className="songRow">
+    <div className="songRow" onClick={(e) => changeTrack(e, track)}>
       <img className="songRow__album" src={track.album.images[0].url} alt="" />
       <div className="songRow__info">
         <h1>{track.name}</h1>
-        <p>
-          {track.artists.map((artist) => artist.name).join(', ')} -{' '}
-          {track.album.name}
-        </p>
+        <p>{track.artists.map((artist) => artist.name).join(', ')}</p>
+        <p>{track.album.name}</p>
       </div>
     </div>
   );
